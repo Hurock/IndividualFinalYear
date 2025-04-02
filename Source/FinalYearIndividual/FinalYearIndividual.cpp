71aa4593bed9d54d379debdb868d0350e013ea3e
Vitantonio Greco <1836528+Hurock@users.noreply.github.com>
2025-04-02 10:11:51 +0100

    ProjectSetup

---
+++ Source/FinalYearIndividual/FinalYearIndividual.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinalYearIndividual.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalYearIndividual, "FinalYearIndividual" );
+ 
